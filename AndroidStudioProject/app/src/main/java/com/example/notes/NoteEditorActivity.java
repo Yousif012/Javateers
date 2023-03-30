add24bfbe8139a6d73399055befeae8930c29057
nctNMSU <nctaylor@nmsu.edu>
2023-03-30 00:27:21 -0600

    started on autosave feature, currently broken

--- AndroidStudioProject/app/src/main/java/com/example/notes/NoteEditorActivity.java
+++ AndroidStudioProject/app/src/main/java/com/example/notes/NoteEditorActivity.java
@@ -1,15 +1,20 @@
 package com.example.notes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 
 public class NoteEditorActivity extends AppCompatActivity {
@@ -56,6 +61,25 @@ public class NoteEditorActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
                 // add your code here
+                try {
+                    String h = DateFormat.format("MM-dd-yyyyy-h-mmssaa", System.currentTimeMillis()).toString();
+                    // this will create a new name everytime and unique
+                    File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+                    // if external memory exists and folder with name Notes
+                    if (!root.exists()) {
+                        root.mkdir(); // this will create folder.
+                    }
+                    File filepath = new File(root, h + ".txt"); // file path to save
+                    FileWriter writer = new FileWriter(filepath);
+                    writer.append(editText.getText().toString());
+                    writer.flush();
+                    writer.close();
+                    String m = "File generated with name " + h + ".txt";
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    editText.setText(e.getMessage().toString());
+                }
             }
         });
     }
